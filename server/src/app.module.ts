75981b7e09c08c906d65742073ec8432f53ff06a
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-17 21:46:53 +0330

    chore: load and validate environment variables using nestjs config package and Joi

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import appConfigSchema from './common/schemas/app-config.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      load: [appConfig],
+      validationSchema: appConfigSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
