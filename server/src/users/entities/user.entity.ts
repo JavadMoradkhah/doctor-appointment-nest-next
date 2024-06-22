007d5c4d863543348ef8079c09c4845d5f33c665
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-22 19:07:06 +0330

    refactor: removing user entity non-authentication-related fields

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -4,7 +4,6 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
 } from 'typeorm';
-import { UserGender } from '../enums/user-gender.enum';
 import { UserRole } from '../enums/user-role.enum';
 
 @Entity('users')
@@ -12,23 +11,11 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', length: 50 })
-  firstName: string;
-
-  @Column({ type: 'varchar', length: 50 })
-  lastName: string;
-
   @Column({ type: 'varchar', length: 11, unique: true })
   phone: string;
 
-  @Column({ type: 'enum', enum: UserGender })
-  gender: UserGender;
-
   @Column({ type: 'enum', enum: UserRole, default: UserRole.PATIENT })
   role: UserRole;
-
-  @Column({ type: 'varchar', length: 255 })
-  password: string;
 
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
