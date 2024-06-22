c3947b949c78d49c588d7b2e84a7a157653239aa
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-22 21:50:21 +0330

    feat: setup cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser(process.env.COOKIE_SECRET));
 
   app.useGlobalPipes(
     new ValidationPipe({
