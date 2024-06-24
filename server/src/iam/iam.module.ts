e081a170ba34bc8c2f6a5bff5a78db617ff0e932
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-24 19:45:59 +0330

    feat: register access token and refresh token config files using jwt module and config module in the iam module

--- server/src/iam/iam.module.ts
+++ server/src/iam/iam.module.ts
@@ -1,11 +1,14 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QUEUE_OTP_SMS } from 'src/iam/iam.constants';
 import { User } from 'src/users/entities/user.entity';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
-import { Otp } from './authentication/entities/otp.entity';
+import accessTokenConfig from './config/access-token.config';
+import refreshTokenConfig from './config/refresh-token.config';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
 import { SmsQueueConsumer } from './iam.process';
@@ -14,6 +17,10 @@ import { SmsQueueConsumer } from './iam.process';
   imports: [
     TypeOrmModule.forFeature([User, Otp]),
     BullModule.registerQueue({ name: QUEUE_OTP_SMS }),
+    JwtModule.registerAsync(accessTokenConfig.asProvider()),
+    JwtModule.registerAsync(refreshTokenConfig.asProvider()),
+    ConfigModule.forFeature(accessTokenConfig),
+    ConfigModule.forFeature(refreshTokenConfig),
   ],
   providers: [
     {
