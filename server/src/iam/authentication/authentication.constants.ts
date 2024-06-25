3c441d1aa2d2529a0b6a5eb1866da4a516227d42
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-25 19:55:42 +0330

    feat: adding some authentication and authorization error messages to the authentication constants module

--- server/src/iam/authentication/authentication.constants.ts
+++ server/src/iam/authentication/authentication.constants.ts
@@ -14,3 +14,18 @@ export const ERR_MSG_INVALID_PHONE_OR_PASSWORD =
   'شماره موبایل یا کلمه عبور نامعتبر است';
 
 export const ERR_MSG_OTP_HAS_NOT_EXPIRED_YET = 'کد تایید هنوز منقضی نشده است';
+
+export const ERR_MSG_OTP_HAS_NOT_BEEN_SENT_OR_EXPIRED =
+  'کد تایید ارسال نشده یا منقضی شده است';
+
+export const ERR_MSG_INVALID_OTP = 'کد تایید وارد شده نامتعبر است';
+
+export const ERR_MSG_LOGIN_REQUIRED = 'وارد حساب کاربری خود شوید';
+
+export const ERR_MSG_ACCOUNT_IS_INACTIVE = 'حساب کاربری شما غیرفعال شده است';
+
+export const ERR_MSG_ACCOUNT_DOES_NOT_EXIST = 'حساب کاربری شما غیرفعال شده است';
+
+export const ERR_MSG_TOKEN_EXPIRED = 'توکن منقضی شده است';
+
+export const ERR_MSG_INVALID_TOKEN = 'توکن نامعتبر است';
