e011f2ab98b2c5ba9397ac5a79641f44f4bcf8c6
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-25 20:26:56 +0330

    feat: create the active user decorator

---
+++ server/src/iam/authentication/decorators/active-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { REQUEST_USER_KEY } from 'src/iam/iam.constants';
+import { ActiveUserData } from 'src/iam/authentication/interfaces/active-user-data.interface';
+
+export const ActiveUser = createParamDecorator(
+  (field: keyof ActiveUserData | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: ActiveUserData = request[REQUEST_USER_KEY];
+    return field ? user?.[field] : user;
+  },
+);
