64dfc5817e2481bd12c9b4cf3fa0d5e11473fefa
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-06-29 23:02:49 +0330

    feat: creating a data source module for TypeOrm for creating migration files

---
+++ server/src/orm.datasource.ts
@@ -0,0 +1,14 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const connectionOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT || 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/db/migrations/*{.ts,.js}'],
+};
+
+export default new DataSource(connectionOptions);
