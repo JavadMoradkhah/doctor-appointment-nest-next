0940300b92c898f2b594b2517502048e1cb0dfe8
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-25 22:51:40 +0330

    feat: create the database config module

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -1,12 +1,5 @@
 export default () => ({
   environment: process.env.NODE_ENV || 'development',
-  database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
-  },
   redis: {
     host: process.env.REDIS_HOST,
     port: parseInt(process.env.REDIS_PORT) || 6379,
