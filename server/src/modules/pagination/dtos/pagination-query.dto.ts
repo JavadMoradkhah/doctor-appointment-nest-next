3e43670fd9d3816988fd5a75da521949c0622c3e
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-26 18:08:52 +0330

    feat: type cast pagination query dto properties to number using class transformer package @Type() decorator

--- server/src/modules/pagination/dtos/pagination-query.dto.ts
+++ server/src/modules/pagination/dtos/pagination-query.dto.ts
@@ -1,12 +1,15 @@
+import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Max } from 'class-validator';
 
 export class PaginationQueryDto {
   @IsPositive()
   @Max(24)
   @IsOptional()
+  @Type(() => Number)
   limit?: number = 10;
 
   @IsPositive()
   @IsOptional()
+  @Type(() => Number)
   page?: number = 1;
 }
