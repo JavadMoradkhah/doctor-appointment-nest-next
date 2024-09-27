1248de9fc88e53defd302349fb58a0669760fd50
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-27 18:04:33 +0330

    refactor: remove the profile entity and move its columns to the user entity

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -2,11 +2,10 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { UserGender } from '../enums/user-gender.enum';
 import { UserRole } from '../enums/user-role.enum';
-import { Profile } from './profile.entity';
 
 @Entity('users')
 export class User {
@@ -19,14 +18,24 @@ export class User {
   @Column({ type: 'enum', enum: UserRole, default: UserRole.PATIENT })
   role: UserRole;
 
+  @Column({ type: 'varchar', length: 50 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 10, unique: true })
+  nationCode: string;
+
+  @Column({ type: 'enum', enum: UserGender })
+  gender: UserGender;
+
+  @Column({ type: 'date' })
+  dateOfBirth: Date;
+
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
 
   @CreateDateColumn()
   joinedAt: Date;
-
-  @OneToOne(() => Profile, (profile) => profile.user, {
-    cascade: ['update'],
-  })
-  profile: Profile;
 }
