905925daa154c4ce00496582dafa3392fdc62148
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-27 18:02:16 +0330

    feat: create the login dto

---
+++ server/src/modules/iam/authentication/dto/login.dto.ts
@@ -0,0 +1,16 @@
+import { IsNumberString, IsString, Length, Matches } from 'class-validator';
+import {
+  ERR_MSG_INVALID_PHONE,
+  REGEX_PHONE,
+} from '../authentication.constants';
+
+export class LoginDto {
+  @IsString()
+  @Length(11, 11)
+  @Matches(REGEX_PHONE, { message: ERR_MSG_INVALID_PHONE })
+  phone: string;
+
+  @IsNumberString()
+  @Length(6, 6)
+  otp: string;
+}
