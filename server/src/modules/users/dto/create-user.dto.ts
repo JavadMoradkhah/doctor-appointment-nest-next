4fce14ff37df5bd75f5a5d84bdad8eb9b341cea3
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-27 18:04:56 +0330

    refactor: create user dto

--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,16 +1,51 @@
-import { IsEnum, IsString, Length, Matches } from 'class-validator';
+import {
+  IsEnum,
+  IsISO8601,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 import {
   ERR_MSG_INVALID_PHONE,
   REGEX_PHONE,
 } from 'src/modules/iam/authentication/authentication.constants';
+import { UserGender } from '../enums/user-gender.enum';
 import { UserRole } from '../enums/user-role.enum';
 
 export class CreateUserDto {
   @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 50)
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
   @Length(11, 11)
   @Matches(REGEX_PHONE, { message: ERR_MSG_INVALID_PHONE })
   phone: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 10)
+  @IsNumberString()
+  nationCode: string;
+
   @IsEnum(UserRole)
+  @IsNotEmpty()
   role: UserRole;
+
+  @IsEnum(UserGender)
+  @IsNotEmpty()
+  gender: UserGender;
+
+  @IsISO8601()
+  @IsNotEmpty()
+  dateOfBirth: Date;
 }
