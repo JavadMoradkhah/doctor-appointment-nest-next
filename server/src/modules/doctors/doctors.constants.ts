43e0c512e8946c0cda3d6aac78f7fb7f6c54a3c7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-29 22:35:09 +0330

    feat: create the doctors constants module

---
+++ server/src/modules/doctors/doctors.constants.ts
@@ -0,0 +1,12 @@
+// Error Messages
+
+export const ERR_MSG_DOCTOR_WAS_NOT_FOUND = 'پزشک مورد نظر یافت نشد';
+
+export const ERR_MSG_DOCTOR_ACCOUNT_WAS_NOT_FOUND =
+  'اطلاعات پزشکی حساب کاربری تکمیل نشده است';
+
+export const ERR_MSG_DOCTOR_PROFILE_ALREADY_EXISTS =
+  'شما قبلا اطلاعات حساب کاربری خود را تکمیل کرده اید';
+
+export const ERR_MSG_GIVEN_USER_IS_NOT_A_DOCTOR =
+  'کاربر مشخص شده یک پزشک نمی باشد';
