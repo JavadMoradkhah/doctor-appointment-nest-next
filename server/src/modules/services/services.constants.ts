bdbcf5f26551e45e5f0bbf364072a2b8b330fee6
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-09-30 17:37:39 +0330

    feat: create the services constants module

---
+++ server/src/modules/services/services.constants.ts
@@ -0,0 +1,6 @@
+// Error Messages
+
+export const ERR_MSG_SERVICE_WAS_NOT_FOUND = 'خدمت مورد نظر یافت نشد';
+
+export const ERR_MSG_SERVICE_UNIQUENESS_VIOLATION =
+  'شما قبلا این خدمت را اضافه کرده اید';
