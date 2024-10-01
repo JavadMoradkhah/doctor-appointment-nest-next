7bb2d9ee4e08d5eb7cef22250ee51d824d400d61
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-01 22:45:46 +0330

    feat: create the datetime constants module

---
+++ server/src/common/constants/datetime.constants.ts
@@ -0,0 +1,7 @@
+// Regex
+
+export const REGEX_VALID_TIME =
+  /^([0-2][0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])$/;
+
+// Error Messages
+export const ERR_MSG_INVALID_TIME = 'زمان داده شده نامعتبر است';
