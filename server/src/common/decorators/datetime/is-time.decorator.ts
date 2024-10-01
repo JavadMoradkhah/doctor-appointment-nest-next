00150d98ca1a672d9a9ce12224466bcb6b53ad05
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-01 22:46:36 +0330

    feat: create the @IsTime() validation decorator

---
+++ server/src/common/decorators/datetime/is-time.decorator.ts
@@ -0,0 +1,24 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+import { isValidTime } from 'src/common/utils/datetime.util';
+
+export function IsTime(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isTime',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(value: any, args: ValidationArguments) {
+          return isValidTime(value);
+        },
+      },
+    });
+  };
+}
