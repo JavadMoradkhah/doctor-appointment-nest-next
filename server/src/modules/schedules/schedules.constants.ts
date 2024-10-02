e788f195b585117baa82e3e509b8dfcc6a6f0008
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-02 18:05:12 +0330

    feat: create the schedules constants module

---
+++ server/src/modules/schedules/schedules.constants.ts
@@ -0,0 +1,9 @@
+// Error Messages
+export const ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION =
+  'شما قبلا این روز کاری را اضافه کرده اید';
+
+export const ERR_MSG_SCHEDULE_WAS_NOT_FOUND = 'روز کاری مورد نظر یافت نشد';
+
+export const ERR_MSG_SCHEDULE_MIN_HOURS = 'روز کاری حداقل باید یک ساعت باشد';
+
+export const ERR_MSG_SCHEDULE_MAX_HOURS = 'روز کاری حداکثر باید ۱۲ ساعت باشد';
