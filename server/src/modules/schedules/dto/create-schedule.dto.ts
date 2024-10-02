c39559b4b184bf2be3412b249b189ad6659f2d60
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-02 18:04:16 +0330

    feat: use the @MaxTimeDifferenceWith() decorator in the create schedule dto

--- server/src/modules/schedules/dto/create-schedule.dto.ts
+++ server/src/modules/schedules/dto/create-schedule.dto.ts
@@ -8,7 +8,12 @@ import {
 } from 'class-validator';
 import { ERR_MSG_INVALID_TIME } from 'src/common/constants/datetime.constants';
 import { IsTime } from 'src/common/decorators/datetime/is-time.decorator';
+import { MaxTimeDifferenceWith } from 'src/common/decorators/datetime/max-time-difference-with.decorator';
 import { MinTimeDifferenceWith } from 'src/common/decorators/datetime/min-time-difference-with.decorator';
+import {
+  ERR_MSG_SCHEDULE_MAX_HOURS,
+  ERR_MSG_SCHEDULE_MIN_HOURS,
+} from '../schedules.constants';
 
 export class CreateScheduleDto {
   @IsNumber()
@@ -26,7 +31,10 @@ export class CreateScheduleDto {
   @IsNotEmpty()
   @IsTime({ message: ERR_MSG_INVALID_TIME })
   @MinTimeDifferenceWith('startAt', 1 * 60 * 60, {
-    message: 'روز کاری حداقل باید یک ساعت باشد',
+    message: ERR_MSG_SCHEDULE_MIN_HOURS,
+  })
+  @MaxTimeDifferenceWith('startAt', 12 * 60 * 60, {
+    message: ERR_MSG_SCHEDULE_MAX_HOURS,
   })
   endAt: string;
 }
