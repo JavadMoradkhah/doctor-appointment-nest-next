63ad1ca5748793f50add2e2a60211e14669a57b3
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-03 20:55:05 +0330

    feat: create the weekday enum

---
+++ server/src/modules/schedules/enums/weekday.enum.ts
@@ -0,0 +1,9 @@
+export enum Weekday {
+  SATURDAY = 'saturday',
+  SUNDAY = 'sunday',
+  MONDAY = 'monday',
+  TUESDAY = 'tuesday',
+  WEDNESDAY = 'wednesday',
+  THURSDAY = 'thursday',
+  FRIDAY = 'friday',
+}
