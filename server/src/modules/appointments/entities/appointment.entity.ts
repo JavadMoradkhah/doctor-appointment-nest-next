3826b9713eb3e568780b23619bab2647ee78bd64
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-03 20:56:48 +0330

    feat: create the appointment entity

---
+++ server/src/modules/appointments/entities/appointment.entity.ts
@@ -0,0 +1,35 @@
+import { IPostgresInterval } from 'postgres-interval';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+} from 'typeorm';
+import { Doctor } from '../../doctors/entities/doctor.entity';
+
+@Entity('appointments')
+@Index(['doctor', 'date'], { unique: true })
+export class Appointment {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => Doctor, {
+    onDelete: 'CASCADE',
+  })
+  doctor: Doctor;
+
+  @RelationId((appointment: Appointment) => appointment.doctor)
+  doctorId: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @Column({ type: 'interval', nullable: true })
+  appointmentsGap: IPostgresInterval;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
