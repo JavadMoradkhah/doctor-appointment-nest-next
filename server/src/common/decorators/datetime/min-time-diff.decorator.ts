27b225dd900d02992bff3d30d972a4b9a9323a45
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-15 19:57:14 +0330

    feat: create the @MinTimeDiff() decorator

---
+++ server/src/common/decorators/datetime/min-time-diff.decorator.ts
@@ -0,0 +1,36 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+import { Duration } from 'src/common/interfaces/duration.interface';
+import { parseTime, durationToSeconds } from 'src/common/utils/datetime.util';
+
+export function MinTimeDiff(
+  property: string,
+  timeDiff: Duration,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'minTimeDiff',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+
+          return (
+            durationToSeconds(parseTime(value) ?? {}) -
+              durationToSeconds(parseTime(relatedValue) ?? {}) >=
+            durationToSeconds(timeDiff)
+          );
+        },
+      },
+    });
+  };
+}
