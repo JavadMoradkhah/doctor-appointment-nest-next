c0bae679f4733300d110ee5e9577688dd94bb860
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-15 20:06:43 +0330

    refactor: schedules service

--- server/src/modules/schedules/schedules.service.ts
+++ server/src/modules/schedules/schedules.service.ts
@@ -1,19 +1,19 @@
 import {
-  BadRequestException,
   ConflictException,
   ForbiddenException,
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { ERR_MSG_DOCTOR_ACCOUNT_WAS_NOT_FOUND } from '../doctors/doctors.constants';
-import { Doctor } from '../doctors/entities/doctor.entity';
+import { FindDoctorProvider } from '../doctors/providers/find-doctor.provider';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import { Schedule } from './entities/schedule.entity';
+import { Weekday } from './enums/weekday.enum';
 import {
   ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION,
-  ERR_MSG_SCHEDULE_WAS_NOT_FOUND,
+  Weekdays,
 } from './schedules.constants';
 
 @Injectable()
@@ -21,37 +21,8 @@ export class SchedulesService {
   constructor(
     @InjectRepository(Schedule)
     private readonly schedulesRepo: Repository<Schedule>,
-    @InjectRepository(Doctor)
-    private readonly doctorsRepo: Repository<Doctor>,
+    private readonly findDoctorProvider: FindDoctorProvider,
   ) {}
-
-  async create(userId: number, createScheduleDto: CreateScheduleDto) {
-    const doctor = await this.doctorsRepo.findOneBy({ userId });
-
-    if (!doctor) {
-      throw new ForbiddenException(ERR_MSG_DOCTOR_ACCOUNT_WAS_NOT_FOUND);
-    }
-
-    const scheduleExists = await this.schedulesRepo.existsBy({
-      doctor: { userId },
-      weekday: createScheduleDto.weekday,
-    });
-
-    if (scheduleExists) {
-      throw new ConflictException(ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION);
-    }
-
-    const schedule = await this.schedulesRepo.save(
-      this.schedulesRepo.create({
-        doctor: { userId },
-        weekday: createScheduleDto.weekday,
-        startAt: createScheduleDto.startAt,
-        endAt: createScheduleDto.endAt,
-      }),
-    );
-
-    return schedule;
-  }
 
   async findAll(userId: number) {
     const schedules = await this.schedulesRepo.find({
@@ -76,17 +47,70 @@ export class SchedulesService {
     return schedule;
   }
 
+  async create(userId: number, createScheduleDto: CreateScheduleDto) {
+    const doctor = await this.findDoctorProvider.findOrFail(userId);
+
+    await this.checkScheduleExists(userId, createScheduleDto.weekday);
+
+    const dayName = Weekdays[createScheduleDto.weekday];
+
+    const schedule = await this.schedulesRepo.save(
+      this.schedulesRepo.create({
+        doctor: { userId: doctor.userId },
+        dayName: dayName,
+        weekday: createScheduleDto.weekday,
+        startsAt: createScheduleDto.startsAt,
+        endsAt: createScheduleDto.endsAt,
+        appointmentsDuration: createScheduleDto.appointmentsDuration,
+        ...(createScheduleDto.breakStartsAt && {
+          breakStartsAt: createScheduleDto.breakStartsAt,
+        }),
+        ...(createScheduleDto.breakStartsAt && {
+          breakEndsAt: createScheduleDto.breakEndsAt,
+        }),
+      }),
+    );
+
+    return schedule;
+  }
+
   async update(
     id: number,
     userId: number,
     updateScheduleDto: UpdateScheduleDto,
   ) {
     const schedule = await this.findOne(id, userId);
-    return await this.schedulesRepo.save({ ...schedule, ...updateScheduleDto });
+
+    if (updateScheduleDto.weekday !== schedule.weekday) {
+      await this.checkScheduleExists(userId, updateScheduleDto.weekday);
+      schedule.weekday = updateScheduleDto.weekday;
+      schedule.dayName = Weekdays[updateScheduleDto.weekday];
+    }
+
+    schedule.appointmentsDuration = updateScheduleDto.appointmentsDuration;
+    schedule.startsAt = updateScheduleDto.startsAt;
+    schedule.endsAt = updateScheduleDto.endsAt;
+    schedule.breakStartsAt =
+      updateScheduleDto.breakStartsAt ?? schedule.breakStartsAt;
+    schedule.breakEndsAt =
+      updateScheduleDto.breakEndsAt ?? schedule.breakEndsAt;
+
+    return await this.schedulesRepo.save(schedule);
   }
 
   async remove(id: number, userId: number) {
     const schedule = await this.findOne(id, userId);
     return await this.schedulesRepo.remove(schedule);
+  }
+
+  async checkScheduleExists(userId: number, weekday: Weekday) {
+    const scheduleExists = await this.schedulesRepo.existsBy({
+      doctor: { userId },
+      weekday: weekday,
+    });
+
+    if (scheduleExists) {
+      throw new ConflictException(ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION);
+    }
   }
 }
