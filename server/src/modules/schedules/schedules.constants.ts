36bff40446a2f52cbeed1977fd6d655d1143f248
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-15 20:07:53 +0330

    feat: add a weekdays constant and some error messages to the schedules constants module

--- server/src/modules/schedules/schedules.constants.ts
+++ server/src/modules/schedules/schedules.constants.ts
@@ -1,9 +1,32 @@
+// Constants
+
+export const Weekdays = {
+  0: 'یکشنبه',
+  1: 'دوشنبه',
+  2: 'سه‌شنبه',
+  3: 'چهارشنبه',
+  4: 'پنجشنبه',
+  5: 'جمعه',
+  6: 'شنبه',
+};
+
 // Error Messages
 export const ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION =
   'شما قبلا این روز کاری را اضافه کرده اید';
 
-export const ERR_MSG_SCHEDULE_WAS_NOT_FOUND = 'روز کاری مورد نظر یافت نشد';
+export const ERR_MSG_SCHEDULE_MIN_DURATION = 'روز کاری حداقل باید یک ساعت باشد';
 
-export const ERR_MSG_SCHEDULE_MIN_HOURS = 'روز کاری حداقل باید یک ساعت باشد';
+export const ERR_MSG_SCHEDULE_MAX_DURATION =
+  'روز کاری حداکثر باید ۱۲ ساعت باشد';
 
-export const ERR_MSG_SCHEDULE_MAX_HOURS = 'روز کاری حداکثر باید ۱۲ ساعت باشد';
+export const ERR_MSG_SCHEDULE_BREAK_START_MIN_DIFF_WITH_START_TIME =
+  'زمان استراحت حداقل باید یک ساعت بعد از شروع روز کاری باشد';
+
+export const ERR_MSG_SCHEDULE_BREAK_TIME_MIN_DURATION =
+  'زمان استراحت حداقل باید 15 دقیقه باشد';
+
+export const ERR_MSG_SCHEDULE_BREAK_TIME_MAX_DURATION =
+  'زمان استراحت حداکثر باید 6 ساعت باشد';
+
+export const ERR_MSG_SCHEDULE_BREAK_END_TIME_BEFORE_SCHEDULE_END_TIME =
+  'زمان استراحت باید قبل از زمان به پابان رسیدن روز کاری باشد';
