b8e3ec568bac0fc972f82278506b278dcd7ca256
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-15 20:00:41 +0330

    feat: apply newly-created time-validation decorators on the schedule creation DTO properties

--- server/src/modules/schedules/dto/create-schedule.dto.ts
+++ server/src/modules/schedules/dto/create-schedule.dto.ts
@@ -1,13 +1,28 @@
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+  ValidateIf,
+} from 'class-validator';
 import { ERR_MSG_INVALID_TIME } from 'src/common/constants/datetime.constants';
 import { IsTime } from 'src/common/decorators/datetime/is-time.decorator';
-import { MaxTimeDifferenceWith } from 'src/common/decorators/datetime/max-time-difference-with.decorator';
-import { MinTimeDifferenceWith } from 'src/common/decorators/datetime/min-time-difference-with.decorator';
+import { MaxTimeDiff } from 'src/common/decorators/datetime/max-time-diff.decorator';
+import { MinTimeDiff } from 'src/common/decorators/datetime/min-time-diff.decorator';
 import { Weekday } from '../enums/weekday.enum';
 import {
-  ERR_MSG_SCHEDULE_MAX_HOURS,
-  ERR_MSG_SCHEDULE_MIN_HOURS,
+  ERR_MSG_SCHEDULE_BREAK_END_TIME_BEFORE_SCHEDULE_END_TIME,
+  ERR_MSG_SCHEDULE_BREAK_START_MIN_DIFF_WITH_START_TIME,
+  ERR_MSG_SCHEDULE_BREAK_TIME_MAX_DURATION,
+  ERR_MSG_SCHEDULE_BREAK_TIME_MIN_DURATION,
+  ERR_MSG_SCHEDULE_MAX_DURATION,
+  ERR_MSG_SCHEDULE_MIN_DURATION,
 } from '../schedules.constants';
+import { IsTimeBefore } from 'src/common/decorators/datetime/is-time-before.decorator';
 
 export class CreateScheduleDto {
   @IsEnum(Weekday)
@@ -22,11 +37,50 @@ export class CreateScheduleDto {
   @IsString()
   @IsNotEmpty()
   @IsTime({ message: ERR_MSG_INVALID_TIME })
-  @MinTimeDifferenceWith('startAt', 1 * 60 * 60, {
-    message: ERR_MSG_SCHEDULE_MIN_HOURS,
-  })
-  @MaxTimeDifferenceWith('startAt', 12 * 60 * 60, {
-    message: ERR_MSG_SCHEDULE_MAX_HOURS,
-  })
+  @MinTimeDiff(
+    'startsAt',
+    { hours: 1 },
+    { message: ERR_MSG_SCHEDULE_MIN_DURATION },
+  )
+  @MaxTimeDiff(
+    'startsAt',
+    { hours: 12 },
+    { message: ERR_MSG_SCHEDULE_MAX_DURATION },
+  )
   endsAt: string;
+
+  @IsString()
+  @IsOptional()
+  @IsTime({ message: ERR_MSG_INVALID_TIME })
+  @MinTimeDiff(
+    'startsAt',
+    { hours: 1 },
+    { message: ERR_MSG_SCHEDULE_BREAK_START_MIN_DIFF_WITH_START_TIME },
+  )
+  breakStartsAt: Date;
+
+  @IsString()
+  @ValidateIf((schedule: CreateScheduleDto) => !!schedule.breakStartsAt)
+  @IsTime({ message: ERR_MSG_INVALID_TIME })
+  @IsTimeBefore('endsAt', {
+    message: ERR_MSG_SCHEDULE_BREAK_END_TIME_BEFORE_SCHEDULE_END_TIME,
+  })
+  @MinTimeDiff(
+    'breakStartsAt',
+    { minutes: 15 },
+    { message: ERR_MSG_SCHEDULE_BREAK_TIME_MIN_DURATION },
+  )
+  @MaxTimeDiff(
+    'breakStartsAt',
+    { hours: 4 },
+    { message: ERR_MSG_SCHEDULE_BREAK_TIME_MAX_DURATION },
+  )
+  breakEndsAt: Date;
+
+  @IsNumber()
+  @IsInt()
+  @Min(5) // 5 minutes
+  @Max(720) // 12 * 60 = 720 minutes
+  @IsNotEmpty()
+  appointmentsDuration: number;
 }
