c3201605c010cb15b21066f3f7b15d26c80af3ab
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-15 19:53:45 +0330

    feat: create the duration interface

---
+++ server/src/common/interfaces/duration.interface.ts
@@ -0,0 +1,8 @@
+export interface Duration {
+  /** The number of hours in the duration */
+  hours?: number;
+  /** The number of minutes in the duration */
+  minutes?: number;
+  /** The number of seconds in the duration */
+  seconds?: number;
+}
