e66056753c147ccf1d719e19b6fd1a0799132738
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-19 20:08:35 +0330

    feat: create the dayoffs constants module

---
+++ server/src/modules/dayoffs/dayoff.constants.ts
@@ -0,0 +1,10 @@
+// Error Message
+
+export const ERR_MSG_DAY_OFF_UNIQUENESS_VIOLATION =
+  'تاریخ های انتخاب شده با تاریخ روز های تعطیل ثبت شده تداخل دارد';
+
+export const ERR_MSG_DAY_OFF_START_DATE_BEFORE_END_DATE =
+  'تاریخ شروع تعطیلات باید قبل از تاریخ پایان باشد';
+
+export const ERR_MSG_DAY_OFF_END_DATE_AFTER_START_DATE =
+  'تاریخ پایان تعطیلات باید بعد از تاریخ شروع باشد';
