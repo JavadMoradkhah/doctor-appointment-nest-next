01c34e04d3516fad760b0298e75241443b2556c5
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-19 20:07:01 +0330

    feat: create the @IsDateAfter() decorator

---
+++ server/src/common/decorators/datetime/is-date-after.decorator.ts
@@ -0,0 +1,34 @@
+import {
+  isDateString,
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+
+export function IsDateAfter(
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isDateAfter',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+
+          if (!isDateString(value) || !isDateString(relatedValue)) {
+            return false;
+          }
+
+          return new Date(value).getTime() > new Date(relatedValue).getTime();
+        },
+      },
+    });
+  };
+}
