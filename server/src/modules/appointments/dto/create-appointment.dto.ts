9e8c1d96bc14eb954de9685d44712d5586cfc1f5
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-19 22:09:41 +0330

    refactor: appointment create and update DTOs

--- server/src/modules/appointments/dto/create-appointment.dto.ts
+++ server/src/modules/appointments/dto/create-appointment.dto.ts
@@ -1,29 +1,8 @@
-import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
-import { IPostgresInterval } from 'postgres-interval';
-import { ERR_MSG_INVALID_TIME } from 'src/common/constants/datetime.constants';
-import { IsTime } from 'src/common/decorators/datetime/is-time.decorator';
-import { MaxTime } from 'src/common/decorators/datetime/max-time.decorator';
-import { MinTime } from 'src/common/decorators/datetime/min-time.decorator';
-import {
-  ERR_MSG_APPOINTMENTS_GAP_MAX_TIME,
-  ERR_MSG_APPOINTMENTS_GAP_MIN_TIME,
-} from 'src/modules/doctors/doctors.constants';
+import { Type } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateAppointmentDto {
-  @IsDateString()
   @IsNotEmpty()
+  @Type(() => Date)
   date: Date;
-
-  @IsString()
-  @IsNotEmpty()
-  @IsTime({ message: ERR_MSG_INVALID_TIME })
-  @MinTime(
-    { hours: 0, minutes: 5, seconds: 0 },
-    { message: ERR_MSG_APPOINTMENTS_GAP_MIN_TIME },
-  )
-  @MaxTime(
-    { hours: 1, minutes: 0, seconds: 0 },
-    { message: ERR_MSG_APPOINTMENTS_GAP_MAX_TIME },
-  )
-  appointmentsGap: IPostgresInterval;
 }
