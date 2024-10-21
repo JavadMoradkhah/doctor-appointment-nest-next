e39e977f4722f594574609bc89cb354a45ed92e5
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-21 22:38:05 +0330

    feat: create the @MaxDateDiff() decorator

---
+++ server/src/common/decorators/datetime/max-date-diff.decorator.ts
@@ -0,0 +1,40 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from 'class-validator';
+
+/**
+ * Checks if two dates difference is <= dateDiff
+ * @param property - The target property name to validate against
+ * @param dateDiff - The maximum number of seconds
+ * @param validationOptions - Validation Options
+ */
+export function MaxDateDiff(
+  property: string,
+  dateDiff: number,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'maxDateDiff',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        validate(date: unknown, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedDate = (args.object as any)[relatedPropertyName];
+
+          return (
+            date instanceof Date &&
+            relatedDate instanceof Date &&
+            date.getTime() - relatedDate.getTime() <= dateDiff * 1000
+          );
+        },
+      },
+    });
+  };
+}
