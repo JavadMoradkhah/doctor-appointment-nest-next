466e07baca237a1b4107c3cc778b422890bc4f80
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-22 21:09:42 +0330

    feat: add an error message to the datetime constants module

--- server/src/common/constants/datetime.constants.ts
+++ server/src/common/constants/datetime.constants.ts
@@ -4,4 +4,7 @@ export const REGEX_VALID_TIME =
   /^([0-2][0-3]|[0-1][0-9]):([0-5][0-9]):([0-5][0-9])$/;
 
 // Error Messages
+
 export const ERR_MSG_INVALID_TIME = 'زمان داده شده نامعتبر است';
+
+export const ERR_MSG_INVALID_DATE = 'تاریخ داده شده نامعتبر است';
