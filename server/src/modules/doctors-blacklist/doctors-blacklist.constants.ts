13dde1ccbfe39cd6e47e44bca3ec5e43f58eef0b
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-22 17:05:01 +0330

    feat: create the doctors blacklist constants module

---
+++ server/src/modules/doctors-blacklist/doctors-blacklist.constants.ts
@@ -0,0 +1,7 @@
+// Error Messages
+
+export const ERR_MSG_DOCTORS_BLACKLIST_MIN_EXPIRATION_TIME =
+  'زمان انقضا حداقل باید برای یک ساعت آینده باشد';
+
+export const ERR_MSG_DOCTORS_BLACKLIST_PATIENT_UNIQUENESS_VIOLATION =
+  'این بیمار قبلا به لیست سیاه اضافه شده است';
