c22877e3375b2a8de4998c32321c2adb1a44dd10
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-28 18:16:44 +0330

    feat: create the redis config module

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -1,10 +1,5 @@
 export default () => ({
   environment: process.env.NODE_ENV || 'development',
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT) || 6379,
-    url: process.env.REDIS_URL,
-  },
   api: {
     sms: process.env.API_KEY_SMS,
   },
