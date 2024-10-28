d1e7c390942b0d6e3c6191bc2afc77071646847f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-28 19:54:11 +0330

    feat: create reservation create DTO

---
+++ server/src/modules/reservations/dto/create-reservation.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  MinDate,
+} from 'class-validator';
+import { IsTime } from 'src/common/decorators/datetime/is-time.decorator';
+
+export class CreateReservationDto {
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
+  doctorId: number;
+
+  @IsDate()
+  @IsNotEmpty()
+  @MinDate(() => new Date(Date.now()))
+  date: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsTime()
+  time: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  reason: string;
+}
