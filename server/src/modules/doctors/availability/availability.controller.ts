a43b8c9774faba70328491c2c9b28ce38ca7e8a6
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-10-28 18:23:35 +0330

    feat: add an endpoint to get a list of doctor available time slots on a given date

--- server/src/modules/doctors/availability/availability.controller.ts
+++ server/src/modules/doctors/availability/availability.controller.ts
@@ -1,17 +1,26 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ParseDatePipe } from 'src/common/pipes/parse-date.pipe';
 import { Roles } from 'src/modules/iam/authorization/decorators/roles.decorator';
 import { UserRole } from 'src/modules/users/enums/user-role.enum';
 import { AvailabilityService } from './availability.service';
 
 @Roles(UserRole.PATIENT)
 @ApiTags('availability')
-@Controller('availability')
+@Controller(':doctorId/availability')
 export class AvailabilityController {
   constructor(private readonly availabilityService: AvailabilityService) {}
 
-  @Get(':doctorId/dates')
+  @Get('dates')
   findDates(@Param('doctorId') doctorId: number) {
     return this.availabilityService.findDates(doctorId);
+  }
+
+  @Get(':date/times')
+  findTimes(
+    @Param('doctorId') doctorId: number,
+    @Param('date', ParseDatePipe) date: Date,
+  ) {
+    return this.availabilityService.findTimes(doctorId, date);
   }
 }
--- server/src/modules/doctors/availability/availability.service.ts
+++ server/src/modules/doctors/availability/availability.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Appointment } from 'src/modules/appointments/entities/appointment.entity';
-import { MoreThanOrEqual, Repository } from 'typeorm';
+import { DataSource, MoreThanOrEqual, Repository } from 'typeorm';
 
 @Injectable()
 export class AvailabilityService {
   constructor(
     @InjectRepository(Appointment)
     private readonly appointmentsRepo: Repository<Appointment>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async findDates(doctorId: number) {
@@ -32,5 +33,28 @@ export class AvailabilityService {
     });
 
     return dates;
+  }
+
+  async findTimes(doctorId: number, date: Date) {
+    const timeSlots = await this.dataSource.manager.query(
+      `
+      SELECT time::time, "appointments"."isAvailable"
+      FROM "appointments"
+      LEFT JOIN "schedules"
+        ON "schedules"."id" = "appointments"."scheduleId"
+      JOIN generate_series(
+        "appointments"."date"::date + "schedules"."startsAt"::time,
+        "appointments"."date"::date + "schedules"."endsAt"::time,
+        concat("schedules"."appointmentsDuration"::smallint, ' minutes')::interval
+      ) AS gs(time) ON TRUE
+      WHERE
+        "appointments"."doctorId" = $1
+        AND "appointments"."date" = $2
+        AND (time::time < "schedules"."breakStartsAt" OR time::time >= "schedules"."breakEndsAt")
+    `,
+      [doctorId, date],
+    );
+
+    return timeSlots;
   }
 }
