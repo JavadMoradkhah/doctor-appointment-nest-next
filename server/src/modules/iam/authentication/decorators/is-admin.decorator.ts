057c799e410aaa25d02286324d76b66d0a0372c1
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-11-05 21:15:32 +0330

    feat: create the @IsAdmin() decorator

---
+++ server/src/modules/iam/authentication/decorators/is-admin.decorator.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { ActiveUserData } from 'src/modules/iam/authentication/interfaces/active-user-data.interface';
+import { REQUEST_USER_KEY } from 'src/modules/iam/iam.constants';
+import { UserRole } from 'src/modules/users/enums/user-role.enum';
+
+export const IsAdmin = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: ActiveUserData = request[REQUEST_USER_KEY];
+    return user.role === UserRole.ADMIN;
+  },
+);
