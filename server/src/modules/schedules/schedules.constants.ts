82b398e4151d1a9a0191595299e49469975321ae
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-11-13 14:17:52 +0330

    feat: create the schedules constants module

---
+++ server/src/modules/schedules/schedules.constants.ts
@@ -0,0 +1,20 @@
+// Error Messages
+
+export const ERR_MSG_SCHEDULE_START_DATE_BEFORE_END_DATE =
+  'تاریخ شروع باید قبل از تاریخ پایان باشد';
+
+export const ERR_MSG_SCHEDULE_END_DATE_AFTER_START_DATE =
+  'تاریخ پایان باید بعد از تاریخ شروع باشد';
+
+export const ERR_MSG_SCHEDULE_MIN_DAYS = 'نوبت دهی حداقل باید یک روز باشد';
+
+export const ERR_MSG_SCHEDULE_MAX_DAYS = 'نوبت دهی حداکثر باید ۳۰ روز باشد';
+
+export const ERR_MSG_SCHEDULE_UNIQUENESS_VIOLATION =
+  'در برنامه نوبت دهی تداخل زمانی وجود دارد';
+
+export const ERR_MSG_SCHEDULE_APPOINTMENTS_MIN_DURATION =
+  'زمان هر نوبت حداقل باید ۵ دقیقه باشد';
+
+export const ERR_MSG_SCHEDULE_APPOINTMENTS_MAX_DURATION =
+  'زمان هر نوبت حداکثر باید ۱ ساعت باشد';
