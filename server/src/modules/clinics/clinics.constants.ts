7d2eca155500ad97bd25f0bd3f53db37262eb322
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-11-14 18:06:26 +0330

    feat: create the clinics constants module

---
+++ server/src/modules/clinics/clinics.constants.ts
@@ -0,0 +1,8 @@
+// Error Messages
+
+export const ERR_MSG_CLINIC_ALREADY_EXISTS = 'اطلاعات کلینیک قبلا ثبت شده است';
+
+export const ERR_MSG_INVALID_TELEPHONE = 'شماره تلفن وارد شده نامعتبر می باشد';
+
+export const ERR_MSG_CLINIC_TELEPHONE_ALREADY_EXISTS =
+  'این شماره تلفن قبلا اضافه شده است';
